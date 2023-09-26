e2db6425a1283aad19fef5942e6464f6e9a3ab8f
fogu <fogu123@yahoo.com>
2023-09-26 12:58:21 -0400

    Prototype_A1

---
+++ Prototype/Assets/C#/LevelHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelHandler : MonoBehaviour
+{
+    public bool open;
+    private Animator ani;
+    public KeyCode interactKey;
+
+    void Start(){
+        ani = gameObject.GetComponent<Animator>();
+    }
+
+    void Update(){
+        if(open == true){
+            ani.SetBool("OpenState", true);
+        }
+        else{
+            ani.SetBool("OpenState", false);
+        }
+    }
+
+    public void OpenDoor(){
+        open = true;
+    }
+
+    public void CloseDoor(){
+        open = false;
+    }
+
+    void OnTriggerStay2D(Collider2D col){
+        if(open == true && col.tag == "Player" && Input.GetKey(interactKey)){
+            NextLevel();
+        }
+    }
+
+
+    void NextLevel(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextScenceIndex = currentSceneIndex + 1;
+        if (nextScenceIndex == SceneManager.sceneCountInBuildSettings){
+            nextScenceIndex = 0;
+        }
+        SceneManager.LoadScene(nextScenceIndex);
+    }
+}
--- Prototype/Assets/C#/TelekinesisController.cs
+++ Prototype/Assets/C#/TelekinesisController.cs
@@ -6,9 +6,10 @@ public class TelekinesisController : MonoBehaviour
 {
     [SerializeField] private GameObject telekinesisIcon;
     [SerializeField] private Camera cam;
-    public bool tele;
     private Vector2 mousePos;
     private const float iconOffsetY = 0.5f;
+    public LayerMask Player;
+    public LayerMask Box;
 
     private void Start() {
         telekinesisIcon.GetComponent<SpriteRenderer>().enabled = false;
@@ -19,15 +20,19 @@ public class TelekinesisController : MonoBehaviour
 
     private void OnMouseOver() {
         if(GameObject.Find("Player").GetComponent<AbilityController>().telekenisis == true){
-            UpdateTelekinesisIcon();
-            if(Input.GetMouseButton(0)){
-            gameObject.tag = "Nonmoveable";
-            tele = true;
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
-            gameObject.transform.position = mousePos;
+                UpdateTelekinesisIcon();
+                if(Input.GetMouseButton(0)){
+                gameObject.tag = "Nonmoveable";
+                gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+                gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+                GameObject.Find("Player").GetComponent<Movement>().enabled = false;
+                GameObject.Find("Player").GetComponent<Rigidbody2D>().velocity = new Vector2(0, GameObject.Find("Player").GetComponent<Rigidbody2D>().velocity.y);
+                gameObject.transform.position = mousePos;
             }
             else{
-            gameObject.tag = "Moveable";
+                GameObject.Find("Player").GetComponent<Movement>().enabled = true;
+                gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+                gameObject.tag = "Moveable";
             }
         }
     }
