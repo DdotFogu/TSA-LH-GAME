7077b6b446698d8982f9b0dcfcf67ef69e33b01e
fogu <fogu123@yahoo.com>
2024-01-28 17:55:14 -0500

    Hud
    added hud elements to show what abilites are avaiable. does show me a bug
    involving the buddy ability enabling all powers

--- Prototype/Assets/C#/AbilityController.cs
+++ Prototype/Assets/C#/AbilityController.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AbilityController : MonoBehaviour
 {
+    public Color transparent;
+    public Color full;
+
     [Header("Magic")]
     public bool telekenisis;
+    public Image telekenisisIcon;
     public bool stasis;
+    public Image stasisIcon;
     public bool invertGravity;
+    public Image GravityIcon;
     public bool littleBuddy;
+    public Image BuddyIcon;
 
     [Header("Conditions")]
     public bool carryingMetal;
@@ -28,5 +36,32 @@ public class AbilityController : MonoBehaviour
         stasis = true;
         invertGravity = true;
         littleBuddy = true;
+    }
+
+    private void Update(){
+        if(!telekenisis){
+            telekenisisIcon.color = transparent;
+        }
+        else{
+            telekenisisIcon.color = full;
+        }
+        if(!stasis){
+            stasisIcon.color = transparent;
+        }
+        else{
+            stasisIcon.color = full;
+        }
+        if(!invertGravity){
+            GravityIcon.color = transparent;
+        }
+        else{
+            GravityIcon.color = full;
+        }
+        if(!littleBuddy){
+            BuddyIcon.color = transparent;
+        }
+        else{
+            BuddyIcon.color = full;
+        }
     }
 }
